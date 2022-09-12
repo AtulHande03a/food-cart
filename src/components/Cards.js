f90fa26ef849885bcc33fa7c190c3210ed3fa36b
Atul Kumar Hande <handeatul03a@gmail.com>
2022-09-12 22:51:48 +0530

    feat :added card and card details components

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,45 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Cardsdata from "./CardsData";
+
 const Cards = () => {
-  return <div>Cards</div>;
+  const [data, setData] = useState(Cardsdata);
+
+  return (
+    <div className="container mt-3">
+      <h2 className="text-center">Add to Cart Project</h2>
+
+      <div className="row">
+        {data.map((element, id) => {
+          return (
+            <>
+              <Card
+                style={{ width: "22rem", border: "none" }}
+                className="mx-2 mt-4 card_style"
+              >
+                <Card.Img
+                  variant="top"
+                  src={element.imgdata}
+                  style={{ height: "16rem" }}
+                  className="mt-3"
+                />
+                <Card.Body>
+                  <Card.Title>{element.rname}</Card.Title>
+                  <Card.Text>Price : ₹ {element.price}</Card.Text>
+                  <div className="button_div d-flex justify-content-center">
+                    <Button variant="primary" className="col-lg-12">
+                      Add to Cart
+                    </Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
